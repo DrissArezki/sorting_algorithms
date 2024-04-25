8dfc7578eb7d9d14d400b37de45e46b224d9f0bf
DrissArezki <driss.arezki5@gmail.com>
2024-04-25 14:03:56 -0700

    0x1B. C - Sorting algorithms & Big O

---
+++ 3-quick_sort.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+
+/**
+ * swap_func - function that swaps two ints
+ * @array: array with elmts
+ * @size: size of array
+ * @a: int one
+ * @b: int twio
+ * Return: void
+ */
+
+void swap_func(int *array, size_t size, int *a, int *b)
+{
+	if (*a != *b)
+	{
+		*a ^= *b;
+		*b ^= *a;
+		*a ^= *b;
+		print_array((const int *)array, size);
+	}
+}
+
+/**
+ * lomuto_part - partition using the pivot
+ * @array: array to use
+ * @size: size of the array
+ * @min: min index of the range
+ * @max: max index
+ *
+ * Return: size_t
+ */
+
+size_t lomuto_part(int *array, size_t size, size_t min, size_t max)
+{
+	int i, j, piv = array[max];
+
+	for (i = j = min; j < max; j++)
+		if (array[j] < piv)
+			swap_func(array, size, &array[j], &array[i++]);
+	swap_func(array, size, &array[i], &array[max]);
+
+	return (i);
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * @array: array to sort
+ * @size: size of the array
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+
