e16667cdac2e8606f4078d95e5a8fef582f7b681
DrissArezki <driss.arezki5@gmail.com>
2024-04-27 03:29:08 -0700

    0x1B. C - Sorting algorithms & Big O

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,19 +16,18 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = i;
-		for (j = i + 1; j < size; j++)
+		for (j = size - 1, k = i + 1; j > i; j--)
 		{
-			if (array[j] < array[min])
+			if (array[j] < array[k])
 			{
-				min = j;
+				k = j;
 			}
 		}
-		if (min != i)
+		if (array[i] > array[k])
 		{
 			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
+			array[i] = array[k];
+			array[k] = temp;
 			print_array(array, size);
 		}
 	}
