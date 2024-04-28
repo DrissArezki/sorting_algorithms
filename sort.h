a514d3404774ed3bfa81e55c79dd5edbbc3eebc4
DrissArezki <driss.arezki5@gmail.com>
2024-04-28 10:07:02 -0700

    0x1B. C - Sorting algorithms & Big O

--- sort.h
+++ sort.h
@@ -1,9 +1,23 @@
-#ifndef _SORT_H
-#define _SORT_H
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
